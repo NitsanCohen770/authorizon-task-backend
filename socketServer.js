930801564c9f85dc4da5be0ffd81490c0a807ccb
nitsancohen770 <nitsancohen770@gmail.com>
2021-07-13 00:02:23 +0300

    admin filtering

--- socketServer.js
+++ socketServer.js
@@ -14,29 +14,71 @@ const io = require('socket.io')(server, {
 const PORT = 5000;
 const NEW_CHAT_MESSAGE_EVENT = 'newChatMessage';
 const NEW_FILTER_QUERY_EVENT = 'newFilterAdded';
+const ADMIN_USER_VALIDATE = 'validateUserAdmin';
+const IS_ADMIN_USER = 'isAdminUser';
 const filtersDatabase = new Filters();
 const messagesDatabase = new Messages();
 
-io.use(
-  socketioJwt.authorize({
-    secret: process.env.AUTH0_CLIENT_SECRET,
-    handshake: true,
-  })
-);
+// io.use(
+//   socketioJwt.authorize({
+//     secret: process.env.AUTH0_CLIENT_SECRET,
+//     handshake: false,
+//   })
+// );
+
+io.use((socket, next) => {
+  const adminList = ['nitsancohen770@gmail.com'];
+  if (adminList.includes(socket.handshake.query.email)) {
+    socket.emit(
+      IS_ADMIN_USER,
+      adminList.includes(socket.handshake.query.email)
+    );
+  }
+  console.log(socket);
+
+  next();
+});
 
 io.on('connection', socket => {
   console.log('connected!');
-  const adminList = ['nitsancohen770@gmail.com'];
   socket.on(NEW_CHAT_MESSAGE_EVENT, data => {
-    messagesDatabase.messages.push(data);
-    messagesDatabase
-      .save() //first find if the filter array is empty if not filter messages and then send expect admin
-      .then(res => io.emit(NEW_CHAT_MESSAGE_EVENT, res.messages));
+    const newMessage = new Messages({ ...data });
+    newMessage.save().then(res =>
+      Filters.find().then(allFilters => {
+        if (allFilters.length === 0) {
+          Messages.find().then(allMessages =>
+            io.emit(NEW_CHAT_MESSAGE_EVENT, allMessages)
+          );
+        }
+        const filters = allFilters.map(filter => filter.keywords);
+        const regex = filters.join('|');
+        Messages.find({
+          body: { $not: { $regex: regex, $options: 'i' } },
+        }).then(filteredMessages =>
+          io.emit(NEW_CHAT_MESSAGE_EVENT, filteredMessages)
+        );
+      })
+    );
   });
 
   socket.on(NEW_FILTER_QUERY_EVENT, filterQuery => {
-    filtersDatabase.filters.push(filterQuery);
-    filtersDatabase.save().then(res => console.log(res));
+    const newFilter = new Filters({ ...filterQuery });
+    newFilter.save().then(res =>
+      Filters.find().then(allFilters => {
+        const filters = allFilters.map(filter => filter.keywords);
+        const regex = filters.join('|');
+        Messages.find({
+          body: { $not: { $regex: regex, $options: 'i' } },
+        }).then(filteredMessages =>
+          io.emit(NEW_CHAT_MESSAGE_EVENT, filteredMessages)
+        );
+      })
+    );
+  });
+
+  socket.on(ADMIN_USER_VALIDATE, ({ userEmail }) => {
+    console.log(userEmail);
+    io.emit(IS_ADMIN_USER, adminList.includes(userEmail));
   });
 });
 
